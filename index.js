b5263371df70d5c803812c4ad7a62972da210e5d
Mirhasan Huseynzade <mirhasan123321@gmail.com>
2019-12-16 15:43:37 +0400

    added clicking and sounds

--- index.js
+++ index.js
@@ -1,15 +1,37 @@
 "use strict";
 
-// this will be array for to add new color by every step
-let gamePattern = [];
 
 let buttonColours = ["red", "blue", "green", "yellow"];
 
+// this will be array for to add new color by every step
+let gamePattern = [];
+let userClickedPattern = [];
+
 // it is the function for to generate random color
-function nextSequence() {
-    let randomNumber = Math.floor((Math.random() * 4)); //creating random numbers
-    let randomChosenColour = buttonColours[randomNumber]; //combine numbers with colors
-    gamePattern.push(randomChosenColour); //push color names to array
-    console.log(gamePattern)
+$(document).keypress(function () {
+    let randomNumber = Math.floor(Math.random() * 4); // reating random numbers
+    let randomChosenColour = buttonColours[randomNumber]; // combine numbers with colors
+
+    gamePattern.push(randomChosenColour); // push color names to array
+
+    let flash = $("#" + randomChosenColour); // choose buttons with same id
+    $(flash).fadeOut(100).fadeIn(100); // flash animation in buttons
+
+    // playing audio
+    playSound(randomChosenColour);
+})
+
+// function for clicking to buttons
+$(".btn").click(function () {
+    let userChosenColour = $(this).attr("id");
+    userClickedPattern.push(userChosenColour);
+
+    // playing audio
+    playSound(userChosenColour);
+})
+
+// audio function
+function playSound(name) {
+    let audio = new Audio("sounds/" + name + ".mp3");
+    audio.play();
 }
-nextSequence()
