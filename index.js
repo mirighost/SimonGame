2b3312a43a86282c28948375b0eb542f9880de04
Mirhasan Huseynzade <mirhasan123321@gmail.com>
2019-12-17 21:27:38 +0400

    finished

--- index.js
+++ index.js
@@ -7,37 +7,18 @@ let buttonColours = ["red", "blue", "green", "yellow"];
 let gamePattern = [];
 let userClickedPattern = [];
 
-var started = true;
+var started = false;
 
 var level = 0; // variable of levels
 
 // started function
 $(document).keypress(function () {
-    if (!!started) {
+    if (!started) {
+        $("#level-title").text("Level " + level);
         nextSequence();
-        started = false;
+        started = true;
     }
-})
-
-// it is the function for to generate random color
-function nextSequence() {
-
-    level++; // chanhing 
-
-    $("#level-title").text("Level " + level); //updating h1 by every level
-
-    let randomNumber = Math.floor(Math.random() * 4); // reating random numbers
-    let randomChosenColour = buttonColours[randomNumber]; // combine numbers with colors
-
-    gamePattern.push(randomChosenColour); // push color names to array
-
-    let flash = $("#" + randomChosenColour); // choose buttons with same id
-    $(flash).fadeOut(100).fadeIn(100); // flash animation in buttons
-
-    // playing audio
-    playSound(randomChosenColour);
-
-}
+});
 
 // function for clicking to buttons
 $(".btn").click(function () {
@@ -48,7 +29,64 @@ $(".btn").click(function () {
     playSound(userChosenColour);
     // animating
     animatePress(userChosenColour);
+
+    checkAnswer(userClickedPattern.length - 1);
 })
+
+// for to check answer
+function checkAnswer(currentLevel) {
+    if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
+        console.log("success");
+
+        if (userClickedPattern.length === gamePattern.length) {
+            setTimeout(function () {
+                nextSequence();
+            }, 1000);
+        }
+    }
+    else {
+        console.log("wrong");
+
+        playSound("wrong");
+
+        $("body").addClass("game-over");
+        setTimeout(function () {
+            $("body").removeClass("game-over");
+        }, 200);
+
+        $("#level-title").text("Game Over, Press Any Key to Restart");
+
+        startOver();
+    }
+}
+
+// start game when it is game over
+function startOver(){
+    level = 0;
+    gamePattern = [];
+    started = false;
+}
+
+// it is the function for to generate random color, flashing, playing audio (MAIN FUNCTION)
+function nextSequence() {
+
+    userClickedPattern = [];
+    level++; // chanhing level
+
+    $("#level-title").text("Level " + level); //updating h1 by every level
+
+    let randomNumber = Math.floor(Math.random() * 4); // reating random numbers
+    let randomChosenColour = buttonColours[randomNumber]; // combine numbers with colors
+
+    gamePattern.push(randomChosenColour); // push color names to array
+
+    $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100); // flash animation in buttons
+
+    // playing audio
+    playSound(randomChosenColour);
+}
+
+
 
 // audio function
 function playSound(name) {
